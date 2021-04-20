349f331cc4f007558978d029aeadf7e0b23850e1
tylercmac <tylercmac@gmail.com>
2021-04-20 11:08:17 -0700

    testing server

--- index.js
+++ index.js
@@ -7,4 +7,14 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'))
+
 const tables = [];
+
+
+app.get('/', (req, res) => {
+    res.send('server live!')
+})
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
