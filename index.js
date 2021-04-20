2bf900d4b967dc9f0f074a35ade6025356b8fc2b
Trevor Eckberg <trevor.eckberg@gmail.com>
2021-04-20 11:10:12 -0700

    update

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const tables = [];
+const waitlist = [];
+
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+
+app.get("/tables", (req, res) => res.sendFile(path.join(__dirname, "tables.html")));
+
+app.get("/reserve", (req, res) => res.sendFile(path.join(__dirname, "reserve.html")));
+
+app.get("/api/tables", (req, res) => res.json(tables));
+
+app.get("/api/waitlist", (req, res) => res.json(waitlist));
